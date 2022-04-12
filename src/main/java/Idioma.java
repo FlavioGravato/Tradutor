3bc3f211a532213c7f41ed07b5e41ef80a7849b6
Flavio Gravato <flavio.gravato@gmail.com>
2022-04-12 12:19:13 +0100

    Added classe Idioma

---
+++ src/main/java/Idioma.java
@@ -0,0 +1,31 @@
+public class Idioma {
+    private String sigla;
+    private String descricao;
+
+    public Idioma(String sigla, String descricao) {
+        this.sigla = sigla;
+        this.descricao = descricao;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String toString(){
+        return String.format("Sigla: %d --- Descricao: %d%n",this.sigla,this.descricao);
+    }
+    
+
+}
